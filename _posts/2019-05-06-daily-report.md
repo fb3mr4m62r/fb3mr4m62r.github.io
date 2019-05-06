a24b280138ab7f1378cdce0be4d3ad30b042c3e2
fb3mr4m62r <fb3mr4m62r@gmail.com>
2019-05-07 02:26:35 +0900

    Add some posts

---
+++ _posts/2019-05-06-daily-report.md
@@ -0,0 +1,11 @@
+---
+title: "오늘 일기"
+excerpt: "발췌 여기 적는거"
+categories:
+  - report
+tags:
+  - job
+last_modified_at: 2019-05-06T01:30
+---
+
+나는 오늘 블로그 세팅을 했음
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "GitHub 블로그 만들기에 대한 초기 생각들",
+        "excerpt":"git , github, jekyll에 대해 아무것도 모를 때 했던 생각들과 시도의 기록을 남겨둔다. 1. 복잡한 생각들 최근에 Python 관련 공부한 것들을 정리하기 위해 기존에 사용하던 네이버 카페와 구글 블로거를 사용했다. 그런데 여러모로 불편한 점이 많았다. 디자인, 가독성, 코드 입력, 카테고리 등등… 뭔가 조금씩 아쉽고 불편했다. 내가 사용하는 방법을 잘 몰라서...","categories": ["blog"],
+        "tags": ["jekyll","ruby"],
+        "url": "http://localhost:4000/blog/blog-github-jekyll-ini-thought/",
+        "teaser":null},{
+        "title": "git 공부 정리 (1): 설치 및 초기설정",
+        "excerpt":"본 문서는 버전관리 프로그램 git에 대해 공부한 기록 (1)이다. git 설치 및 초기설정에 관련한 내용을 공부했다. 1. 머리말 Jekyll을 사용한 GitHub 블로그 만들기에 대한 자료를 조사하던 중, 버전 관리 프로그램인 git의 사용방법을 모르면 전혀 진행이 안된다는 사실을 알았다. 왜냐하면 설명 대부분이 Command Line Interface 상에서 git을 사용하는 내용을 담고있기 때문이다....","categories": ["tool"],
+        "tags": ["git","github","jekyll"],
+        "url": "http://localhost:4000/tool/tool-study-git-1/",
+        "teaser":null},{
+        "title": "오늘 일기",
+        "excerpt":"나는 오늘 블로그 세팅을 했음  ","categories": ["report"],
+        "tags": ["job"],
+        "url": "http://localhost:4000/report/daily-report/",
+        "teaser":null}]

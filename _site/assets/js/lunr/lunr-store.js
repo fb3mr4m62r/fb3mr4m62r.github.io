c19df75e45842bca5003a087053bfcf6b4e8a5a1
fb3mr4m62r <fb3mr4m62r@gmail.com>
2019-05-07 21:14:37 +0900

    add some posts

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,16 +1,11 @@
 var store = [{
-        "title": "GitHub 블로그 만들기에 대한 초기 생각들",
-        "excerpt":"git , github, jekyll에 대해 아무것도 모를 때 했던 생각들과 시도의 기록을 남겨둔다. 1. 복잡한 생각들 최근에 Python 관련 공부한 것들을 정리하기 위해 기존에 사용하던 네이버 카페와 구글 블로거를 사용했다. 그런데 여러모로 불편한 점이 많았다. 디자인, 가독성, 코드 입력, 카테고리 등등… 뭔가 조금씩 아쉽고 불편했다. 내가 사용하는 방법을 잘 몰라서...","categories": ["blog"],
-        "tags": ["jekyll","ruby"],
-        "url": "http://localhost:4000/blog/blog-github-jekyll-ini-thought/",
-        "teaser":null},{
-        "title": "git 공부 정리 (1): 설치 및 초기설정",
-        "excerpt":"본 문서는 버전관리 프로그램 git에 대해 공부한 기록 (1)이다. git 설치 및 초기설정에 관련한 내용을 공부했다. 1. 머리말 Jekyll을 사용한 GitHub 블로그 만들기에 대한 자료를 조사하던 중, 버전 관리 프로그램인 git의 사용방법을 모르면 전혀 진행이 안된다는 사실을 알았다. 왜냐하면 설명 대부분이 Command Line Interface 상에서 git을 사용하는 내용을 담고있기 때문이다....","categories": ["tool"],
-        "tags": ["git","github","jekyll"],
-        "url": "http://localhost:4000/tool/tool-study-git-1/",
-        "teaser":null},{
         "title": "오늘 일기",
         "excerpt":"나는 오늘 블로그 세팅을 했음  ","categories": ["report"],
+        "tags": ["job"],
+        "url": "http://localhost:4000/report/daily-report/",
+        "teaser":null},{
+        "title": "테스트",
+        "excerpt":"5월7일 테스트 typora : 마크다운에디터 마크다운에디터로 작성후 웹용페이지를 보여주는 기능을 함 마크다운: HTML같은 문법 git : 폴더에서 미리보기를 할경우 상위폴더에가서 우클릭 girbash here 열고 터미널이 열리면 다움명령어로 미리보기가 가능하다 윈도우일경우에는 : /c/Windows/System32/chcp 65001 이명령어는 ANCI를 UTF-8로 변환하라는명령어 인데 해줘야함 bundle exec jekyll serve 로컬서버가 시작되면 웹페이지에서 다음주소에서 확인가능 localhost:4000 Ruby:...","categories": ["report"],
         "tags": ["job"],
         "url": "http://localhost:4000/report/daily-report/",
         "teaser":null}]
